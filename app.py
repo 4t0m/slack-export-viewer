118f56dcd6082b99f3770bbe1a1df39292857f23
Adom Hartell <adomhartell@gmail.com>
2018-01-06 17:25:28 -0800

    hardcode archive location

--- app.py
+++ app.py
@@ -1,12 +1,76 @@
 import flask
 
 
+######################## HAXXXXX
+
+import os
+import webbrowser
+
+import click
+import flask
+
+from slackviewer.app import app
+from slackviewer.archive import \
+    extract_archive, \
+    get_users, \
+    get_channels, \
+    compile_channels
+
+
+def envvar(name, default):
+    """Create callable environment variable getter
+
+    :param str name: Name of environment variable
+    :param default: Default value to return in case it isn't defined
+    """
+    return lambda: os.environ.get(name, default)
+
+
+def flag_ennvar(name):
+    return os.environ.get(name) == '1'
+
+
+def configure_app(app, archive):
+    # app.debug = debug
+    # if app.debug:
+    #     print("WARNING: DEBUG MODE IS ENABLED!")
+    app.config["PROPAGATE_EXCEPTIONS"] = True
+
+    path = extract_archive(archive)
+    user_data = get_users(path)
+    channel_data = get_channels(path)
+    channels = compile_channels(path, user_data, channel_data)
+
+    top = flask._app_ctx_stack
+    top.channels = channels
+
+def main(port, archive, ip, no_browser, debug):
+    if not archive:
+        raise ValueError("Empty path provided for archive")
+
+    configure_app(app, archive, debug)
+
+    if not no_browser:
+        webbrowser.open("http://{}:{}".format(ip, port))
+
+    app.run(
+        host=ip,
+        port=port
+    )
+
+
+
+######################## HAXXXXX
+
+
+
 app = flask.Flask(
     __name__,
     template_folder="templates",
     static_folder="static"
 )
 
+configure_app(app, './archive.zip')
 
 @app.route("/channel/<name>/")
 def channel_name(name):
